22ff964c9584ea4fe0b3bf667cc25e7c2497963d
Donald <roronoadonald3@gmail.com>
2026-05-02 01:16:20 +0000

    troisieme commit oft

--- plugins/security.js
+++ plugins/security.js
@@ -5,10 +5,21 @@ const env = require('../config/env');
 
 async function securityPlugin(app) {
   await app.register(helmet);
-  await app.register(cors, {
-    origin: env.corsOrigin,
-    credentials: true
-  });
+  // If CORS origin is wildcard in env, allow reflect-origin so credentials can still be used.
+  const corsOptions = {
+    credentials: true,
+  };
+
+  if (env.corsOrigin && env.corsOrigin !== '*') {
+    corsOptions.origin = env.corsOrigin;
+  } else {
+    // reflect the request origin (allows credentials) when wildcard is configured
+    corsOptions.origin = (origin, cb) => {
+      cb(null, true);
+    };
+  }
+
+  await app.register(cors, corsOptions);
 }
 
 module.exports = fp(securityPlugin);
